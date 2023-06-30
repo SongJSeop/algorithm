ed0ed75b8b5a956d9d4158b030f20dcc7fb3e358
SongJSeop <wnstjq7979@gmail.com>
2023-06-30 15:15:10 +0900

    lv1_삼총사 문제 풀이

---
+++ Programers/lv1_threeFriends.py
@@ -0,0 +1,20 @@
+# 프로그래머스 Lv1. 삼총사
+
+def solution(number):
+    answer = 0
+
+    for i in range(len(number) - 2):
+        for j in range(i + 1, len(number) - 1):
+            for k in range(j + 1, len(number)):
+                if number[i] + number[j] + number[k] == 0:
+                    answer += 1
+
+    return answer
+
+
+number1 = [-2, 3, 0, 2, -5]
+print(solution(number1))
+number2 = [-3, -2, -1, 0, 1, 2, 3]
+print(solution(number2))
+number3 = [-1, 1, -1, 1]
+print(solution(number3))
