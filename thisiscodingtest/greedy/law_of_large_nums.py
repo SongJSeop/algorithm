d5d4a63331449b82b74459cb3d32f00b42cfef3c
SongJSeop <wnstjq7979@gmail.com>
2024-04-08 15:24:41 +0900

    큰 수의 법칙 문제 다른 풀이

--- thisiscodingtest/greedy/law_of_large_nums.py
+++ thisiscodingtest/greedy/law_of_large_nums.py
@@ -5,16 +5,10 @@ numList.sort()
 first = numList[n - 1]
 second = numList[n - 2]
 
-result = 0
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+# count는 가장 큰 숫자(first)가 나오는 횟수
+count = int(m / (k + 1)) * k  # 'm / (k + 1)'의 몫만큼 반복하며, 그 반복 하나하나에 k개 만큼 큰 숫자가 들어감
+count += m % (k + 1)  # 나머지도 더해줌
 
-print(result)
+result = 0
+result += count * first  # 가장 큰 숫자가 더해진 값
+result += (m - count) * second  # 두 번재 숫자가 더해진 값
