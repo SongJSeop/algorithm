50d5cf059a836195f1b007e1c62bd6799e6f92a7
송준섭 <wnstjq7979@gmail.com>
2024-04-30 20:58:51 +0900

    시각 문제 책 모범 답안 추가

--- thisiscodingtest/implementation/time.py
+++ thisiscodingtest/implementation/time.py
@@ -9,3 +9,15 @@ for i in range(hour+1):
         count += 1575
 
 print(count)
+
+# 책 모범 답안
+# h = int(input())
+#
+# count = 0
+# for i in range(h + 1):
+#     for j in range(60):
+#         for k in range(60):
+#             if '3' in str(i) + str(j) + str(k):
+#                 count += 1
+#
+# print(count)
