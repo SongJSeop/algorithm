af0d30139008d403ac20f3b0f9153cf464028102
송준섭 <wnstjq7979@gmail.com>
2024-04-30 21:22:38 +0900

    왕실의 나이트 문제 책 모범 답안 추가

--- thisiscodingtest/implementation/royal_knight.py
+++ thisiscodingtest/implementation/royal_knight.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
+
 # 내가 푼 풀이
 def validatePosition(column, row):
     return (97 <= column <= 104) and (1 <= row <= 8)
 
 
 position = input()
+startTime = datetime.now()
 column, row = ord(position[0]), int(position[1])
 
 canMove = 0
@@ -23,3 +27,26 @@ for move in moveList:
         canMove += 1
 
 print(canMove)
+print(datetime.now() - startTime)
+
+# 책 모범 답안
+# input_data = input()
+# startTime = datetime.now()
+# row = int(input_data[1])
+# column = int(ord(input_data[0])) - int(ord('a')) + 1
+#
+# steps = [
+#     (-2, -1), (-1, -2), (1, -2), (2, -1),
+#     (2, 1), (1, 2), (-1, 2), (-2, 1)
+# ]
+#
+# result = 0
+# for step in steps:
+#     next_row = row + step[0]
+#     next_column = column + step[1]
+#
+#     if (1 <= next_row <= 8) and (1 <= next_column <= 8):
+#         result += 1
+#
+# print(result)
+# print(datetime.now() - startTime)
