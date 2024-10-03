9ac3a4a3ed61feb8057c993973de6689aafb1fe5
송준섭 <wnstjq7979@gmail.com>
2024-10-03 17:08:16 +0900

    두 배열 원소 교체 모범 답안 풀이 추가

--- thisiscodingtest/sorting/trade_element.py
+++ thisiscodingtest/sorting/trade_element.py
@@ -1,12 +1,29 @@
-# 내가 푼 풀이
+# # 내가 푼 풀이
+# n, k = map(int, input().split())
+#
+# arrayA = list(map(int, input().split()))
+# arrayB = list(map(int, input().split()))
+#
+# for i in range(k):
+#     arrayA.sort()
+#     arrayB.sort()
+#     arrayA[0], arrayB[n-1] = arrayB[n-1], arrayA[0]
+#
+# print(sum(arrayA))
+
+# 모범 답안
 n, k = map(int, input().split())
 
-arrayA = list(map(int, input().split()))
-arrayB = list(map(int, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
 
 for i in range(k):
-    arrayA.sort()
-    arrayB.sort()
-    arrayA[0], arrayB[n-1] = arrayB[n-1], arrayA[0]
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+    else:
+        break
 
-print(sum(arrayA))
+print(sum(a))
