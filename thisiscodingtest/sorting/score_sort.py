89332f22bacb7d6227c9e132fa34526c931162cc
송준섭 <wnstjq7979@gmail.com>
2024-10-03 16:43:27 +0900

    성적순 정렬 모범 답안 추가

--- thisiscodingtest/sorting/score_sort.py
+++ thisiscodingtest/sorting/score_sort.py
@@ -14,3 +14,16 @@ def sortStandard(data):
 array.sort(key=sortStandard)
 for tup in array:
     print(tup[0], end=" ")
+
+# # 모범 답안
+# n = int(input())
+#
+# array = []
+# for i in range(n):
+#     input_data = input().split()
+#     array.append((input_data[0], int(input_data[1])))
+#
+# array = sorted(array, key=lambda student: student[1])
+#
+# for student in array:
+#     print(student[0], end=" ")
